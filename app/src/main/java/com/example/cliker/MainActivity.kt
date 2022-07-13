aea5ac3a6a96ac178cb1d77f3c2798d5acf3206a
ShadowZilot <e.ponomaryow2017@yandex.ru>
2022-07-13 19:34:39 +0300

    Добавлена возможность сохранения результата на долго

--- app/src/main/java/com/example/cliker/MainActivity.kt
+++ app/src/main/java/com/example/cliker/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.cliker
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
@@ -9,23 +11,33 @@ private const val key_count = "count"
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
+    private lateinit var storage : SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        savedInstanceState?.let {
-            count = it.getInt(key_count)
+        storage = this.getSharedPreferences(
+            key_count,
+            Context.MODE_PRIVATE
+        )
+        count = score()
+        if (count != 0) {
             findViewById<TextView>(R.id.TextCount).text = count.toString()
         }
     }
 
     fun onCLick(view: View) {
         count += 1
+        saveScore()
         findViewById<TextView>(R.id.TextCount).text = count.toString()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(key_count, count)
+    private fun score() = storage.getInt(key_count, 0)
+
+    private fun saveScore() {
+        storage
+            .edit()
+            .putInt(key_count, count)
+            .apply()
     }
 }
