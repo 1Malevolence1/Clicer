8025d66844714cfb8877e39efb23c461c9208cd2
Administrator <danil36rusdanil@mail.ru>
2022-07-13 19:09:31 +0300

    Сохронение при повороте

--- app/src/main/java/com/example/cliker/MainActivity.kt
+++ app/src/main/java/com/example/cliker/MainActivity.kt
@@ -1,26 +1,42 @@
 package com.example.cliker
 
-import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+private const val key_Count = "count"
 
 class MainActivity : AppCompatActivity() {
-    var count = 0
+
+   private var count = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        savedInstanceState?.let {
+            count =  it.getInt(key_Count)
+            val Textcount = findViewById<TextView>(R.id.TextCount)
+            Textcount.text = count.toString()
+
+        }
 
 
 
     }
-    fun onCLick(view: View){
+
+    fun onCLick(view: View) {
         val Textcount = findViewById<TextView>(R.id.TextCount)
         count = count + 1
         Textcount.text = count.toString()
         val TextButton = findViewById<TextView>(R.id.addCounter)
         TextButton.text = "Жмяк"
 
-        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(key_Count, count)
+    }
 }
